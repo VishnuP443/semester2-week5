a87760dc1b75f77938a5d72e018458fc52c69ba2
Vishnu Pandaraparambath <lfjt0879@leeds.ac.uk>
2026-02-25 19:16:37 +0000

    More optimised ws solution

--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -9,7 +9,6 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
 
 // ASCII of 0-8 is 48-57
 // ASCII of A-F is 65-70
@@ -24,32 +23,35 @@ int main(void)
 	fgets(buffer, 100, stdin);
 	sscanf(buffer, "%s", hex);
 	int length = strlen(hex);
-	// Power is used in the calculation to work out the decimal eqivalent of the position of the hex character
-	int power = length - 1;
 
 	for (int k = 0; k < length; k++)
 	{
-		if (!('0' <= hex[k] && hex[k] <= '9') && !('A' <= hex[k] && hex[k] <= 'F') && !('a' <= hex[k] && hex[k] <= 'f'))
-		{
-			printf("Error: Invalid Hexadecimal\n");
-		}
 
 		if ('0' <= hex[k] && hex[k] <= '9') // Checks if the hex is 0-9
 		{
-			decimal += pow(16, power) * (hex[k] - 48); // subtracts 48 so that 0 scales to 0 and 1-9 scale accordingly
-
+			// subtracts 48 so that 0 scales to 0 and 1-9 scale accordingly
+			decimal = (decimal * 16) + (hex[k] - 48);
+			// Shifts the whole hex by a place value to ensure they end up in the correct place
 		}
 
-		if ('A' <= hex[k] && hex[k] <= 'F') // Checks if the hex is A-F
+		else if ('A' <= hex[k] && hex[k] <= 'F') // Checks if the hex is A-F
 		{
-			decimal += pow(16, power) * (hex[k] - 55); // Subtracts 55 so that A scales to 10 and B C .. etc. scale accordingly
+			// Subtracts 55 so that A scales to 10 and B C .. etc. scale accordingly
+			decimal = (decimal * 16) + (hex[k] - 55);
+			// Shifts the whole hex by a place value to ensure they end up in the correct place
 		}
 
-		if ('a' <= hex[k] && hex[k] <= 'f') 
+		else if ('a' <= hex[k] && hex[k] <= 'f') // Checks if the hex is a-f (lower case)
 		{
-			decimal += pow(16, power) * (hex[k] - 87); //subtracts 87 so that a scales to 10 and b c ... etc accordingly
+			// subtracts 87 so that a scales to 10 and b c ... etc accordingly
+			decimal = (decimal * 16) + (hex[k] - 87);
+			// Shifts the whole hex by a place value to ensure they end up in the correct place
 		}
-		power -= 1;
+
+		else
+		{
+			printf("Error: Invalid Hexadecimal\n");
+		}
 	}
 
 	// print the decimal result
